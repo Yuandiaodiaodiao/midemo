70678861fa7f52742e9c77addb8b4c7eacceb752
Yuandiaodiaodiao <qq295087430@outlook.com>
2019-12-11 21:28:20 +0800

    change addJavascriptInterface

--- android/app/src/main/java/cn/yuandiaodiaodiao/midemo/MainActivity.java
+++ android/app/src/main/java/cn/yuandiaodiaodiao/midemo/MainActivity.java
@@ -1,34 +1,18 @@
 package cn.yuandiaodiaodiao.midemo;
 
 import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
-import android.net.Uri;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebViewClient;
+import android.webkit.JavascriptInterface;
 import android.widget.Button;
 import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.github.lzyzsd.jsbridge.BridgeHandler;
-import com.github.lzyzsd.jsbridge.BridgeUtil;
-import com.github.lzyzsd.jsbridge.BridgeWebView;
-import com.github.lzyzsd.jsbridge.CallBackFunction;
-import com.github.lzyzsd.jsbridge.Message;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
     public Button button1;
     public WebView webView1;
     public EditText edittext;
-//    public String url = "http://192.168.137.1:8888";
+    //    public String url = "http://192.168.137.1:8888";
     public String url = "http://yuandiaodiaodiao.cn:8888";
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,19 +33,12 @@ public class MainActivity extends AppCompatActivity {
         button1 = (Button) findViewById(R.id.button);
 
         webView1 = (WebView) findViewById(R.id.webView);
-        webView1.setWebViewClient(new MyWebViewClient(webView1));
-
+        webView1.getSettings().setJavaScriptEnabled(true);
+        webView1.addJavascriptInterface(new JSNative(this, webView1), "JSnative");
 
         edittext = (EditText) findViewById(R.id.textView);
         edittext.setText(url);
-//使用jsbridge
-//        webView1.registerHandler("submitFromWeb", new BridgeHandler() {
-//            @Override
-//            public void handler(String data, CallBackFunction function) {
-//                Toast.makeText(getApplicationContext(), "msg is " + data, Toast.LENGTH_SHORT).show();
-//                function.onCallBack("submitFromWeb exe, response data from Java");
-//            }
-//        });
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,41 +59,24 @@ public class MainActivity extends AppCompatActivity {
         webView1.loadUrl(url);
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    public class MyWebViewClient extends WebViewClient {
 
-        public MyWebViewClient(WebView webView) {
-//            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-            webView.getSettings().setJavaScriptEnabled(true);
-//            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+    public class JSNative {
+        private WebView webView;
+
+        private JSNative(Context context, WebView webView) {
+            this.webView = webView;
         }
 
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            String url = request.getUrl().toString();
-
-            try {
-                url = URLDecoder.decode(url, "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            if (url.startsWith("xyz")) { // 如果是返回数据
-
-                String[] payload = url.split("//");
-                Toast.makeText(getApplicationContext(), "msg=" + payload[1], Toast.LENGTH_SHORT).show();
-//                Gson json=new Gson();
-//                json.toJson(payload[1]);
-//                Toast.makeText(getApplicationContext(), "url=" + json.toString(), Toast.LENGTH_SHORT).show();
-
-                return true;
-            } else {
-                return super.shouldOverrideUrlLoading(view, request);
-            }
+        @JavascriptInterface
+        public void say(String content) {
+            //收到say
+            Toast.makeText(getApplicationContext(), "js say=" + content, Toast.LENGTH_SHORT).show();
+            //返回调用js
+            String call = "javascript:java2js(\' receive \')";
+            new Handler(Looper.getMainLooper()).post(() -> webView.loadUrl(call));
         }
 
     }
-
 }
 
 
--- web/static/api.js
+++ web/static/api.js
@@ -1,15 +1,21 @@
-function buttonCall() {
+function jscalljava() {
 
-    var a = document.getElementById("input1").value;
-
-    var frameDiv = document.createElement("iframe");//创建一个标签
-    var message = {"text": a}
-    frameDiv.setAttribute('style','width: 0px; height: 0px;');
-    frameDiv.src = "xyz://" + JSON.stringify(message)
-    var bodyFa = document.getElementById("container1");
-    bodyFa.appendChild(frameDiv);
-    setTimeout(()=>{bodyFa.removeChild(frameDiv)},100)
+    let a = document.getElementById("input1").value;
+    JSnative.say(a);
 
 
+    // var frameDiv = document.createElement("iframe");//创建一个标签
+    // var message = {"text": a}
+    // frameDiv.setAttribute('style','width: 0px; height: 0px;');
+    // frameDiv.src = "xyz://" + JSON.stringify(message)
+    // var bodyFa = document.getElementById("container1");
+    // bodyFa.appendChild(frameDiv);
+    // setTimeout(()=>{bodyFa.removeChild(frameDiv)},100)
 
+
+}
+
+function java2js(msg) {
+    let b=document.getElementById("button1")
+    b.innerHTML=msg
 }
